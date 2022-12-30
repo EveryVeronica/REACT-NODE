ef62f262747f8c47b717c458170382cb1c3d2bf4
veronica <veronica.in.th@gmail.com>
2022-12-30 22:46:15 +0700

    แก้ใขไหม่

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import './App.css';
-import Quotes from './components/Quotes';
-import LayoutHeader from './components/LayoutHeader';
-
+import "./App.css";
+import Quotes from "./components/Quotes";
 
 
-import Login from './components/Login'; 
-import Home from './components/Home';
-import Firebase from './services/Firebase'; 
+import LayoutHeader from "./components/LayoutHeader";
 
-
-
+import Firebase from "./services/Firebase";
 
 function App() {
-
   const [user, setUser] = useState(null);
-  useEffect(() => {
-    Firebase.auth().onAuthStateChanged(user => {
-      setUser(user)
-    })
-  },[])
-  console.log(user);
-  
 
+  useEffect(() => {
+    Firebase.auth().onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  console.log(user);
 
   return (
     <div className="App">
-
-<LayoutHeader />
-      { user ? <Home user={user}/>  : <Login />}
-
-
-   
-
-    
+      {user ? <LayoutHeader user={user} /> :<LayoutHeader user={null} /> }
     </div>
   );
 }
--- frontend/src/components/LayoutHeader.js
+++ frontend/src/components/LayoutHeader.js
@@ -1,27 +1,56 @@
-import React , { useState } from 'react'
-
-import './LayoutHeader.css';
 
 
-function LayoutHeader () {
+import "./LayoutHeader.css";
+import { auth } from "../services/Firebase";
+import { signInWithGoogle } from '../services/Firebase';
+
+function LayoutHeader({user}) {
 
 
+  if (user) {
+    return (
+      <div className="LayoutHeader-header">
+          <div className="left">
+        <img src={user.photoURL} alt="a" />
+        <button>{user.displayName}</button>
+     
+      
+          </div>
 
-    return ( 
-        <div className="LayoutHeader-header">
-            <div className='left'>
-                <input type="text" />
-            </div>
-
-            <div className='right'>
-                <input type="text" />
-                <input type="text" />
-                <button type="text">login</button>
-            </div>
-                   
+        <div className="right">
+        <button onClick={() => auth.signOut()}>Sign Out</button>
         </div>
-    );
+    </div>
+  );
+  } else {
+
+
+
+    return (
+      
+      <div className="LayoutHeader-header">
+      <div className="left">
+  
+      </div>
+  
+    <div className="right">
+    <button onClick={signInWithGoogle}>signInWithGoogle</button>
+    </div>
+  </div>
+
+
+    )
+
+
+
+
+
+
+
+  }
+
+
+
 }
 
 export default LayoutHeader;
-
