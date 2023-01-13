ddb758b4ec8bd0a55aa9c0109806a023e9ceb752
veronica <veronica.in.th@gmail.com>
2023-01-13 14:48:33 +0700

    ok

--- frontend/src/components/SpreadsOnline/GroupChat.js
+++ frontend/src/components/SpreadsOnline/GroupChat.js
@@ -5,7 +5,16 @@ function GroupChat() {
   return (
     <div className={styles.grid_container}>
     <div className={styles.item1}>1</div>
-    <div className={styles.item2}>2</div>
+          <div className={styles.item2}>
+
+              
+
+              <input className={styles.id} type="text"/>
+
+
+
+
+    </div>
     <div className={styles.item3}>3</div>  
   
   </div>
