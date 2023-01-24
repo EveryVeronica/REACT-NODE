0cb94e3c59631f70d6462ff3fb02f9f13f73ae2c
veronica <veronica.in.th@gmail.com>
2023-01-24 15:00:31 +0700

    ok สำเร็จ

--- frontend/src/components/Entry/Spreadsheets.js
+++ frontend/src/components/Entry/Spreadsheets.js
@@ -1,37 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import InsertsText from '../Insert/InsertsText';
-import ListText from '../List/ListText';
+import React, { useMemo, useState } from "react";
+import InsertsText from "../Insert/InsertsText";
+import ListText from "../List/ListText";
 
 function Spreadsheets() {
+  const [Lists, setLists] = useState([]); // lists text
+  const [ListsRow, setListsRow] = useState([]); // ListsRow
 
-  const [Lists, setLists] = useState([]);// lists text
 
-  const [ListsRow, setListsRow] = useState([]);// lists text
+    if (Lists.length >= 5) {
+      setListsRow([...ListsRow, Lists]);
+      setLists([]);
+    }
 
-  if (Lists.length >= 5) {
-   const el = Lists.map((item => <input
-      type="text"
-      value={item.text}
-      onChange={()=>{}}
-   />))
-   setListsRow([...ListsRow,el])
-   setLists([])
-  }
 
+  const Renders = useMemo(() => {
+    let arr = [];
+    for (let index = 0; index < ListsRow.length; index++) {
+      let row = ListsRow[index];
+      let element = row.map((item) => (
+        <li key={item.id}>
+          <input type="text" value={item.text} onChange={() => {}} />
+        </li>
+      ));
+
+      let addEl = (
+        <ul key={index}>
+          {element}
+
+          <button
+            type="text"
+            onClick={() => {
+              //  console.log(ListsRow[index]); //ตำแหน่ง arr
+              let fi = ListsRow.filter((item, i) => i !== index);
+              setListsRow(fi);
+            }}
+          >
+            ลบ
+          </button>
+        </ul>
+      );
+
+      arr.push(addEl);
+    }
+
+    return arr;
+  }, [ListsRow]);
 
   return (
     <div>
-         <p>debug:{Lists.map((item)=> item.text)}</p>
-      <InsertsText ReceiveInput={(r) => {
-        setLists([...Lists, r])
-      }} />
+      <p>debug:{Lists.map((item) => item.text)}</p>
+      <InsertsText
+        ReceiveInput={(r) => {
+          setLists([...Lists, r]);
+        }}
+      />
       <ListText Lists={Lists} />
-      
 
-   {ListsRow}
-   
+      {Renders}
     </div>
-  )
+  );
 }
 
-export default Spreadsheets
+export default Spreadsheets;
