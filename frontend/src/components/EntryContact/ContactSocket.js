7d28057a1cc8acc1bdfb1b660bd02748fb1c9517
veronica <veronica.in.th@gmail.com>
2023-01-25 16:44:45 +0700

    ok

--- frontend/src/components/EntryContact/ContactSocket.js
+++ frontend/src/components/EntryContact/ContactSocket.js
@@ -1,9 +1,16 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import io from "socket.io-client";
 function ContactSocket({token}) {
 
     const [ContactID, setContactID] = useState(""); //เก็บ user login WithGoogle
-  //  const [message, setMessage] = useState(""); //เก็บ user login WithGoogle
+    const [message, setMessage] = useState(""); //เก็บ user login WithGoogle
+  const [receive, setReceive] = useState(""); //เก็บ user login WithGoogle
+  const [room, setRoom] = useState(""); //เก็บ user login WithGoogle
+
+
+
+  const inputSetRoomRef = useRef(null);
+  const inputTextRef = useRef(null);
 
 
     const socket = useMemo(() => {
@@ -14,25 +21,117 @@ function ContactSocket({token}) {
           });
       return setSocket
     }, [token]);
+  
+  
+    useMemo(() => {
+      socket.emit("config_room", "1");
+      socket.on("receive_config", (data) => {
+          setContactID(data)
+      });
+
+      socket.on("receive_message", (data) => {
+        setReceive(data);
+      });
 
 
 
 
-   useMemo(() => {
-        socket.emit("config_room", "1");
-        socket.on("receive_config", (data) => {
-            setContactID(data)
-        });
+
     }, [socket]);
-    
+  
+
+  
+  
+
+
+  useMemo(() => {
+
+    if (room != "") {
+      console.log("setroom"+room);
+      socket.emit("join_room", room);
+  
+    }
+
+    }, [room]);
+  
+  
+    useEffect(() => {
+      sendMessage()
+    }, [message]);
+  
+
+
+
+  
+
+
+  
+
+
+
+    //ส่งข้อความ
+  const sendMessage = () => {
+    if (room) {
+      console.log("sendMessage"+room);
+      socket.emit("send_message", { message, room });
+    }
+  };
+  
+
+
+
+
+
+
+
 
 
   return (
       <div>
-         
 
-          <p>Debug: {token}</p>
-  {ContactID}
+           <input
+        ref={inputTextRef}
+        type="text"
+        id="message"
+        name="message"
+      />
+
+      
+<button onClick={() => {
+
+
+        setMessage(inputTextRef.current.value)
+        
+}}>senmsg</button>
+
+     <input
+        ref={inputSetRoomRef}
+        type="text"
+      />
+
+
+      <button onClick={() => {
+
+        if (inputSetRoomRef.current.value != "") {
+          setRoom(inputSetRoomRef.current.value)
+        } else {
+          if (ContactID) {
+            socket.emit("join_room", ContactID);
+            inputSetRoomRef.current.placeholder = ContactID
+            setRoom(ContactID)
+          }
+
+        }
+        
+      }}>ตั่งค่าห้อง</button>
+
+
+          {/* <p>Debug: {token}</p>
+           {ContactID} */}
+      
+      <p>Message: {message}</p>
+      <p>Receive: {receive}</p>
+
     </div>
   )
 }
