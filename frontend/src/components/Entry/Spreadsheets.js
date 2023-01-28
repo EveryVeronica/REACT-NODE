2490472a3c59510afc42678e188e960ec5b2fa9e
veronica <veronica.in.th@gmail.com>
2023-01-28 13:33:42 +0700

    ok

--- frontend/src/components/Entry/Spreadsheets.js
+++ frontend/src/components/Entry/Spreadsheets.js
@@ -1,6 +1,10 @@
 import React, { useMemo, useState } from "react";
 import InsertsText from "../Insert/InsertsText";
 import ListText from "../List/ListText";
+
+import styles from "./Spreadsheets.module.css";
+
+
 import { v4 as uuidv4 } from "uuid";
 function Spreadsheets({user,Input}) {
   const [Lists, setLists] = useState([]); // lists text
@@ -82,6 +86,63 @@ useMemo(() => {
   }, [Input]);
   return (
     <div>
+
+
+
+
+
+
+
+<div className={styles.container_fluid}>
+      <div className={styles.spread_grid}>
+
+
+          <div className={styles.room_id}>1</div>
+          <div  className={styles.new_item}>2</div>
+          <div className={styles.status}>3</div>
+          <div className={styles.control}>4</div>
+          <div  className={styles.sheet}>5</div>
+          <div  className={styles.confirm}>6</div>
+          <div  className={styles.user_online}>7</div>
+         
+
+          
+
+   
+
+
+        </div>
+     
+     
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
       <p>debug:{Lists.map((item) => item.ListText.text)}</p>
       <InsertsText
